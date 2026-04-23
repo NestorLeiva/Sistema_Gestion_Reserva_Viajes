82bf795c81b790fd2d73b19f483dff8585ff42f9
NestorLeiva <nestorjoel2@gmail.com>
2026-04-23 14:20:32 -0600

    nuevas funciones

---
+++ SistemaViajes/Controllers/EmpleadoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaViajes.Models;
+
+public class EmpleadoController : Controller
+{
+    private readonly AppDbContext _context;
+    public EmpleadoController(AppDbContext context) { _context = context; }
+
+    public IActionResult Index() => View(_context.Empleados.ToList());
+
+    public IActionResult Crear() => View();
+
+    [HttpPost]
+    public IActionResult Crear(Empleado empleado)
+    {
+        _context.Empleados.Add(empleado);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+}
